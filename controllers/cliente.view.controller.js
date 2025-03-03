8a488cb5bc74c6bc6a759e02ee382004cd52e6b4
Carlos-SaulPM <carlossaulpazmaldonado@gmail.com>
2025-03-03 08:50:12 -0600

    Se agrego el paginado a la vista, pero falta hacer dinamico los page-item

--- controllers/cliente.view.controller.js
+++ controllers/cliente.view.controller.js
@@ -1,12 +1,24 @@
 const { clienteBusiness } = require("../business");
 
 const obtenerClientes = async (req, res) => {
-  let clientes = await clienteBusiness.obtenerClientes();
+let pagina = Number(req.query.pagina) || 1;
+  let limite = Number(req.query.limite) || 10;
+
+  if (pagina <= 0 && limite <= 0) {
+    console.log("Ingresa una pagina o limite valido");
+    res.status(404).json({error: "Pagina o limite invalido"})
+    return;
+  }
+
+  let clientes = await clienteBusiness.obtenerClientes(pagina, limite);
   if (!clientes) {
     res.render("partials/error404");
     return;
   }
-  res.render("home/index", { clientes });
+  
+  res.render("home/index", { clientes: clientes.lista, pagina: clientes.pagina, totalpaginas: clientes.totalPaginas });
+
+
 };
 
 const eliminarCliente = async (req, res) => {
